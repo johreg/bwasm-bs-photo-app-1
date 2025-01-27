a5cc12e759f2cbe30f518558119b6d618a1206c7
Johan Regnström <johan@joretec.cloud>
2025-01-27 23:17:38 +0100

    WIP

---
+++ wasm-bs-photo-app-1/DataModels/MongoDbUserImageMetadataModel.cs
@@ -0,0 +1,50 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace auth_api_1.Data
+{
+
+    public class MongoDbUserImageMetadataModel
+    {
+
+        public int Id { get; set; }
+
+        public long OwnerUserId { get; set; }
+
+        public string OriginalName { get; set; }
+        public string CurrentName { get; set; }
+
+        public string UserDescription { get; set; }
+
+        public int SizeKB { get; set; }
+
+
+        public DateTime ImageUploadedDatetime { get; set; }
+
+        public string CameraType { get; set; }
+
+        public string UploadSource { get; set; }
+
+
+        public string Folder { get; set; }
+
+        public List<Competition> Competitions { get; set; }
+
+        public string NrOfViews { get; set; }
+    }
+
+    public class Competition
+    {
+        public string CompetitionId { get; set; }
+
+        public string Status { get; set; }
+
+
+        public string Approval { get; set; }
+
+        public int Votes { get; set; }
+
+        public string MyMotivation { get; set; }
+    }
+}
---
+++ wasm-bs-photo-app-1/Pages/MyImagesTest2Api.razor.cs
@@ -0,0 +1,91 @@
+﻿using auth_api_1.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using System.Net.Http.Json;
+
+namespace wasm_bs_photo_app_1.Pages
+{
+    public partial class MyImagesTest2Api
+    {
+        bool expanded = false;
+        bool isUploading = false;  // To track the upload state
+        private string? selectedUrlSnippet;
+      //  ImageBlobFolderStructure? folderStructure;
+        List<string?>? allImageFolders;
+        Dictionary<string, bool>? folderExpandedState = new Dictionary<string, bool>();
+        string? userobjectid;
+        string? tenantid;
+        string? currentbase64Image;
+    //    ImageMetadataResponse imageMetadata;
+        string disabledButtonText = "Uploading...";
+        string enabledButtonText = "Upload Images";
+        HttpClient? _httpClient;
+        string selectedFolder = "";
+
+       // FluentInputFile? myFileByBuffer = default!;
+        int? progressPercent;
+        string? progressTitle;
+        bool IsCanceled;
+        bool fluentNavLinkClicked = false;
+
+        Dictionary<int, string> Files = new();
+
+        string userObjectIdDebug = "userdataobjectidtest2"; //userdataobjectidtest1
+
+        // private IDialogReference? _dialog;
+
+        //    [Inject]
+        //    protected IDialogService? DialogService { get; set; }
+
+
+        [Inject]
+        private NavigationManager NavigationManager { get; set; } = default!;
+
+
+        private void NavigateToImage(int id)
+        {
+            NavigationManager.NavigateTo($"/myimagestest1/myimage{id}");
+        }
+
+
+        protected override async Task OnInitializedAsync()
+        {
+            #region _debug
+#if DEBUG
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri("https://localhost:44336/");
+            userobjectid = userObjectIdDebug;
+            tenantid = "1";
+#else
+            _httpClient = HttpCl;
+            var authState = await AuthenticationStateProvider.GetAuthenticationStateAsync();
+            userobjectid = authState.User.Claims.FirstOrDefault(c => c.Type == "oid")?.Value; // User ID
+            tenantid = authState.User.Claims.FirstOrDefault(c => c.Type == "tid")?.Value;
+#endif
+            #endregion
+
+            await ReloadFolderStructureAsync(userobjectid, tenantid, _httpClient);
+        }
+
+        private async Task ReloadFolderStructureAsync(string userobjectid, string tenantid, HttpClient httpClient)
+        {
+            var queryString = $"?TenantId={tenantid}&UserObjectId={userobjectid}";
+            var folderStructure = await httpClient.GetFromJsonAsync<MongoDbUserImageMetadataModel>($"JoretecTestApi/GetMetadata1");
+        //    allImageFolders = folderStructure?.Folders?.Select(f => f.Name).Where(name => name != null).ToList();
+
+            /*
+            folderExpandedState = new Dictionary<string, bool>();
+
+            if (folderStructure?.Folders != null)
+            {
+                foreach (var folder in folderStructure.Folders)
+                {
+                    folderExpandedState[folder.Name] = false;
+                }
+            }
+            */
+
+        }
+
+    }
+}
